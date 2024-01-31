b5733402411d94d8a173f48ecc3667e3e34e8ead
husseinahmad7 <h7osanna.xyc@gmail.com>
2024-01-31 17:03:05 +0300

    fix sitemap

--- paradome/urls.py
+++ paradome/urls.py
@@ -4,7 +4,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-
+from Domes.sitemap import DomesSitemap
+from posts.sitemap import PostsSitemap
+sitemaps = {
+    'dome': DomesSitemap,
+    'post': PostsSitemap,
+}
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('HusseinAh.urls')),
@@ -23,6 +28,8 @@ urlpatterns = [
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    path('sitemap.xml', sitemaps, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+
 
 ]
 
