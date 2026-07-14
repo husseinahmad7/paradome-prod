4399eda374756a40883ebd14f0fb1adf93221328
hussein ahmad <h7osanna.xyc@gmail.com>
2026-07-15 00:40:40 +0300

    Fix portfolio static fallback on PythonAnywhere

--- manage.py
+++ manage.py
@@ -2,12 +2,17 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import dotenv
+
+try:
+    import dotenv
+except ImportError:
+    dotenv = None
 
 
 def main():
     """Run administrative tasks."""
-    dotenv.read_dotenv()
+    if dotenv is not None:
+        dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paradome.settings')
     try:
         from django.core.management import execute_from_command_line
